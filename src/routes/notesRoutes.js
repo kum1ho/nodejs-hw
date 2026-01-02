441efb19dfd52b28494baa43158f69f371ad345e
Vladislaw <kumiho2708@gmail.com>
2026-01-02 18:37:14 +0200

    Add user authentication with JWT, sessions, and cookies for task 04

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -15,13 +15,15 @@ import {
   createNoteSchema,
   updateNoteSchema,
 } from '../validations/notesValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = express.Router();
+router.use(authenticate);
 
-router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
-router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', celebrate(createNoteSchema), createNote);
-router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
-router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.get('/', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/', celebrate(createNoteSchema), createNote);
+router.patch('/:noteId', celebrate(updateNoteSchema), updateNote);
+router.delete('/:noteId', celebrate(noteIdSchema), deleteNote);
 
 export default router;
